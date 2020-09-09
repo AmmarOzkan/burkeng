1b97a8679bb44e92197c907365f6e4988eccc893
AmmarOzkan <61326832+AmmarOzkan@users.noreply.github.com>
2020-09-09 15:34:36 +0300

    Şurada oyun motoruna benzeyen kodcuklar yapalım şöyle

---
+++ bureng.h
@@ -0,0 +1,570 @@
+#pragma once
+
+#ifndef BURENG
+#define BURENG
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <thread>
+
+struct tValBool {
+	bool b1, b2;
+};
+
+struct Texturee {
+	sf::Texture texture;
+	int height, width;
+	std::string id;
+};
+
+struct f2 {
+	float f1, f2;
+};
+
+struct valueXY {
+	float x, y;
+};
+
+void setTexturee(Texturee* urTexture, int width, int height, std::string id, sf::String filePath) {
+	urTexture->width = width;
+	urTexture->height = height;
+	urTexture->id = id;
+	std::cout << std::endl << "Texture:" << urTexture->id << " yukleme durumu: " << urTexture->texture.loadFromFile(filePath);
+}
+
+/// <summary>
+/// Pencereni senin için hazýrlar
+/// </summary>
+/// <param name="window">Pencerenin Adresi</param>
+/// <param name="frameRate">Penceredeki Yenilenme Hýzý</param>
+void setWindow(sf::RenderWindow& window, int frameRate) {
+	window.setActive(true);
+	window.setFramerateLimit(frameRate);
+	window.setMouseCursorVisible(false);
+}
+
+/// <summary>
+/// Fontu ayarlar ve font ayarlanýp ayarlanamadýðýný komut penceresine yazdýrýr.
+/// Eðer bir hata meydana geldiyse bunu komut penceresinde yazdýrýr.
+/// </summary>
+/// <param name="filePath">Dosya Yolu</param>
+/// <param name="id">Komut penceresinde yazýlacak olan ismi</param>
+/// <returns>Yeni fontu döndürür.</returns>
+sf::Font setFont(sf::String filePath, std::string id) {
+	sf::Font font;
+	std::cout << std::endl << "Font " << id << " Yukleme Durumu: " << font.loadFromFile("res/fonts/batmfo__.ttf");
+	if (!font.loadFromFile("res/fonts/batmfo__.ttf")) {
+		std::cout << std::endl << "Font " << id << " yuklenmesinde bir hata meydana geldi.";
+
+	}
+	return font;
+}
+
+class gameObject {
+private:
+	valueXY pos, scale;
+	sf::Texture texture;
+	Texturee texturee;
+	sf::RenderWindow* window;
+	bool scaleSetted, posSetted;
+public:
+	/// <summary>
+	/// Oyun objesinin pozisyonunu büyüklüðünü vs ayarlar. Hoj bir sýnýftýr.
+	/// </summary>
+	/// <param name="filePath">Texture'ýn olduðu dosya yolu</param>
+	/// <param name="x">X pozisyonu</param>
+	/// <param name="y">Y pozisyonu</param>
+	/// <param name="scaleX">X geniþleme katsayýsý</param>
+	/// <param name="scaleY">Texture'ýn y'inin ne kadar büyütüleceði</param>
+	/// <param name="textureHeight">Texture dikey pixel sayýsý</param>
+	/// <param name="textureWidth">Texture dikey pixel sayýsý</param>
+	/// <param name="id">Komut penceresinde yazýlacak olan ismi</param>
+	/// <param name="yourWindow">Çizilecek olan ekran deðiþkeninin adresi</param>
+	gameObject(sf::String filePath, float x, float y, float scaleX, float scaleY, int textureHeight, int textureWidth, std::string id, sf::RenderWindow* yourWindow) {
+		scaleSetted = true;
+		posSetted = true;
+		texturee.height = textureHeight; texturee.width = textureWidth; texturee.id = id;
+		std::cout << std::endl << "Texture:" << texturee.id << " yukleme durumu: " << texturee.texture.loadFromFile(filePath);
+		pos.x = x; pos.y = y;
+		scale.x = scaleX;
+		scale.y = scaleY;
+		window = yourWindow;
+	}
+	/// <summary>
+	/// Oyun objesinin pozisyonunu büyüklüðünü vs ayarlar. Hoj bir sýnýftýr.
+	/// </summary>
+	/// <param name="urTexturee">Texturee deðiþkeni</param>
+	/// <param name="x">X pozisyonu</param>
+	/// <param name="y">Y pozisyonu</param>
+	/// <param name="scaleX">X geniþleme katsayýsý</param>
+	/// <param name="scaleY">Y geniþleme katsayýsý</param>
+	/// <param name="yourWindow">Çizilecek olan ekran deðiþkeninin adresi</param>
+	gameObject(Texturee urTexturee, float x, float y, float scaleX, float scaleY, sf::RenderWindow* yourWindow) {
+		scaleSetted = true;
+		posSetted = true;
+		std::cout << std::endl << "Texture " << urTexturee.id << " ekleniyor.";
+		texturee = urTexturee;
+		pos.x = x; pos.y = y;
+		scale.x = scaleX;
+		scale.y = scaleY;
+		window = yourWindow;
+	}
+	/// <summary>
+	/// Objeyi ekrana çizer
+	/// </summary>
+	void draw() {
+		sf::Sprite sprite;
+		sprite.setTexture(texturee.texture);
+		if (scaleSetted) {
+			sprite.setScale(scale.x, scale.y);
+		}if (posSetted) {
+			sprite.setPosition(pos.x, pos.y);
+		}
+		else {
+			sprite.setPosition(10, 10);
+		}
+		window->draw(sprite);
+	}
+	/// <summary>
+	/// Pozisyonu ayarlar
+	/// </summary>
+	/// <param name="x">Pozisyon X</param>
+	/// <param name="y">Pozisyon Y</param>
+	valueXY* position() {
+		return &pos;
+	}valueXY* getScale() {
+		return &scale;
+	}Texturee* getTexturee() {
+		return &texturee;
+	}
+};
+
+//FOR PHYSICS
+
+///Dir
+enum Dir { LEFT, RIGHT, UP, DOWN, NONE };
+
+/// <summary>
+/// Çarpýþma kontrolcüsü verilerini tutan deðiþken
+/// </summary>
+struct Trigger {
+	float posX, posY, height, width;
+	std::string id;
+};
+
+/// <summary>
+/// Çarpýþmayla alakalý olaylarý kontrol eden yapan sýnýf
+/// </summary>
+class TriggerClass {
+public:
+	/// <summary>
+	/// Çarpýþýyormu Çarpýþmýyormu
+	/// </summary>
+	/// <param name="fTrigger">Ýlk trigger</param>
+	/// <param name="sTrigger">Ýkinci trigger</param>
+	/// <returns>çarpýþýp çarpýþmadýðýný true false olarak döndürür</returns>
+	bool isColliding(Trigger fTrigger, Trigger sTrigger) {
+		bool xCol = fTrigger.posX + fTrigger.width >= sTrigger.posX && sTrigger.posX + sTrigger.width >= fTrigger.posX;
+		bool yCol = fTrigger.posY + fTrigger.height >= sTrigger.posY && sTrigger.posY + sTrigger.height >= fTrigger.posY;
+		return xCol && yCol;
+	}
+
+	/// <summary>
+	/// Trigger deðiþkenini bizim için gameObject deðiþkenine göre hazýrlar
+	/// </summary>
+	/// <param name="trigger">Trigger deðiþkeninin adresi</param>
+	/// <param name="urObject">Oyun objesi</param>
+	void setTrigger(Trigger* trigger, gameObject* urObject) {
+		trigger->posX = urObject->position()->x;
+		trigger->posY = urObject->position()->y;
+		trigger->height = urObject->getTexturee()->height * urObject->getScale()->y;
+		trigger->width = urObject->getTexturee()->width * urObject->getScale()->x;
+	}
+}triggerClass;
+
+/// <summary>
+/// Fizik verilerini tutan deðiþken (Þimdilik tek þey var ama daha çok þey ekleyecem)
+/// </summary>
+struct PhyVal {
+	float gravity;
+};
+
+/// <summary>
+/// PhyVal deðiþkenini bizim için ayarlar
+/// </summary>
+/// <param name="urVal">Deðiþkenin adresi</param>
+/// <param name="gravity">Yerçekimi</param>
+void setPhyVal(PhyVal* urVal, float gravity) {
+	urVal->gravity = gravity;
+}
+
+//Bir dizide kaç tane trigger olduðunu belirtmemiz istenir.
+//Bu þekilde kütüphaneyi yapan deðil kütüphaneyi kullanan dizilerin kaç tane olduðunu belirtmiþ olur.
+#ifndef TRIGGERCONTROL
+#error Kac tane trigger control oldugunu define TRIGGERCONTROL [sayi] olarak belirlemelisin
+#else
+
+/// <summary>
+/// Triggerlarýn hepsini kontrol edebilmek için gerekli olan deðiþken.
+/// </summary>
+struct Triggers {
+	Trigger* trigger[TRIGGERCONTROL];
+	std::string* id[TRIGGERCONTROL];
+	int nextTrigger;
+};
+
+/// <summary>
+/// Triggers'ý bizim için ayarlar
+/// </summary>
+/// <param name="urTriggers">Deðiþkenin adresi</param>
+void setTriggers(Triggers* urTriggers) {
+	urTriggers->nextTrigger = 0;
+}
+/// <summary>
+/// Triggers deðiþkenine yeni bir trigger ekleme iþlemini gerçekleþtirir
+/// </summary>
+/// <param name="urTriggers">Deðiþkenin adresi</param>
+/// <param name="urTrigger">Eklenecek trigger'ýn adresi</param>
+/// <param name="id">Yazýsal id (Çarpýþmalarda neye çarpýp çarpmadýðýný kontrol etmeye yarýyor)</param>
+void addNewTrigger(Triggers* urTriggers, Trigger* urTrigger, std::string* id) {
+	if (urTriggers->nextTrigger == TRIGGERCONTROL) {
+		std::cout << std::endl << "maksimum triggers sayisina ersildi";
+	}
+	else {
+		urTriggers->trigger[urTriggers->nextTrigger] = urTrigger;
+		urTriggers->id[urTriggers->nextTrigger] = id;
+		urTriggers->nextTrigger++;
+	}
+}
+
+/// <summary>
+/// Fizik olaylarýný içinde bulunduran gameObject
+/// </summary>
+class physicsObject {
+private:
+	gameObject* object;
+	Trigger trigger;
+	bool physicsActive;
+	PhyVal val;
+	valueXY force;
+	Triggers triggerControl;
+public:
+	/// <summary>
+	/// Kontrol edilecek trigger olduðunda kontrol edileceklere ekler
+	/// </summary>
+	/// <param name="urTrigger">Eklenecek trigger'ýn idsi</param>
+	/// <param name="id">Trigger'ýn yazýsal idsi</param>
+	void addControl(Trigger* urTrigger, std::string* id) {
+		addNewTrigger(&triggerControl, urTrigger, id);
+	}
+
+	/// <summary>
+	/// Kendi trigger'ýný gameObjecte göre ayarlar.
+	/// </summary>
+	void setTrigger() {
+		triggerClass.setTrigger(&trigger, object);
+	}
+	/// <summary>
+	/// Physics Object oluþturulurken kullandýðým hep olan bir start fonksiyonu. Kullanýcýnýn kullanmasýna gerek yok.
+	/// </summary>
+	void fixStart() {
+		setTriggers(&triggerControl);
+		setTrigger();
+		force.x = 0;
+		force.y = 0;
+	}
+
+	/// <summary>
+	/// physicsObject oluþturucusu
+	/// </summary>
+	/// <param name="urObject">gameObjectin adresi</param>
+	/// <param name="physicsIsActive">fiziði olucakmý olmayacakmý (sonradan deðiþtirilebilir)</param>
+	/// <param name="gravity">yerçekimi</param>
+	/// <param name="id">oluþturulurken komut satýrýnda kontorl için yazýcak olan id</param>
+	physicsObject(gameObject* urObject, bool physicsIsActive, float gravity, std::string id)
+		: object(urObject), physicsActive(physicsIsActive)
+	{
+		fixStart();
+		setPhyVal(&val, gravity);
+		std::cout << std::endl << "Fizik Objesi: " << id << " olusturuldu.";
+	}
+	/// <summary>
+	/// physicsObject oluþturucusu
+	/// </summary>
+	/// <param name="urObject">gameObjectin adresi</param>
+	/// <param name="physicsIsActive">fiziði olucakmý olmayacakmý (sonradan deðiþtirilebilir)</param>
+	/// <param name="urVal">Fizik deðiþkeni varsa yerçekimi verisi vs girmek yerine bu girilir</param>
+	/// <param name="id">oluþturulurken komut satýrýnda kontorl için yazýcak olan id</param>
+	physicsObject(gameObject* urObject, bool physicsIsActive, PhyVal urVal, std::string id)
+		: object(urObject), val(urVal), physicsActive(physicsIsActive)
+	{
+		fixStart();
+		std::cout << std::endl << "Fizik Objesi: " << id << " olusturuldu.";
+	}
+
+	/// <summary>
+	/// Objeyi çizer
+	/// </summary>
+	void draw() {
+		object->draw();
+	}
+
+	/// <summary>
+	/// 2 Çarpýþmada 2. objenin 1. objenin neresinde olduðunu döndürür
+	/// </summary>
+	/// <param name="urTrigger">Trigger 1</param>
+	/// <param name="objectTrigger">Trigger 2</param>
+	/// <returns>nerede olduðunu döndürür</returns>
+	Dir where(Trigger urTrigger, Trigger objectTrigger) {
+		if (urTrigger.posX > objectTrigger.posX) return RIGHT;
+		else if (urTrigger.posX + urTrigger.width < objectTrigger.posX + objectTrigger.width) return LEFT;
+		else if (urTrigger.posY > objectTrigger.posY) return UP;
+		else if (urTrigger.posY + urTrigger.height < objectTrigger.posY + objectTrigger.height) return RIGHT;
+		else return NONE;
+	}
+
+	/// <summary>
+	/// Obje bir objeyle çarpýþýyormu
+	/// </summary>
+	/// <param name="oObject">Kontrol edilecek objenin Trigger deðiþkeni</param>
+	/// <returns>Çarpýþýyomu çarpýþmýyomu</returns>
+	bool isColliding(Trigger oObject) {
+		oObject.id = "Dýþ Obje";
+		trigger.id = "Burka";
+		bool test = triggerClass.isColliding(oObject, trigger);
+		return test;
+
+	}
+
+	/// <summary>
+	/// Fizik aktifmi deðilmi
+	/// </summary>
+	/// <param name="isActive">akitfmi?</param>
+	void setPhysics(bool isActive) {
+		physicsActive = isActive;
+	}
+
+	/// <summary>
+	/// Genel id kontrolüyle çarpýþma.
+	/// Herhangi bir çimenle çarpýþtýmý?
+	/// Herhangi bir topla çarpýþtýmý?
+	/// </summary>
+	/// <param name="willControl">Kontrol edilecek Triggers id'si</param>
+	/// <returns>Çarpýþýyomu?</returns>
+	bool control(std::string willControl) {
+		bool control = false;
+		for (int i = 0; i < triggerControl.nextTrigger; i++) {
+			if (isColliding(triggerControl.trigger[i][0]) && triggerControl.id[i][0] == willControl || willControl == "") {
+				control = true;
+			}
+		}
+		return control;
+	}
+
+	/// <summary>
+	/// Güvenli bir þekilde x pozisyonunda haraket iþlemi.
+	/// Eðer ilerlerken parametredeki id ile bir obje çarpýþtýysa eklemeyi durdurur ve eklenmemiþ hale getirir.
+	/// </summary>
+	/// <param name="w">Ýlerlenecek birim sayýsý</param>
+	/// <param name="perf">Performans sayýsý. Kaçar kaçar kontrol edeceði yani</param>
+	/// <param name="willControl">Kontrol edilecek id</param>
+	/// <returns></returns>
+	tValBool addX(float w, float perf, std::string willControl) {
+		tValBool b = { false, false };
+		if (w > 0) {
+			for (float i = 0; i < w; i += perf) {
+				float prevPos = object->position()->x;
+				object->position()->x += perf;
+				if (control(willControl)) {
+					//Burda çarpýþtý ve çarpýþmamýþ hale geri döndü.
+					b.b1 = true;
+					object->position()->x -= perf;
+					break;
+				}
+			}
+		}
+		else if (w < 0) {
+			for (float i = 0; i > w; i -= perf) {
+				float prevPos = object->position()->x;
+				object->position()->x -= perf;
+				if (control(willControl)) {
+					//Burda çarpýþtý ve çarpýþmamýþ hale geri döndü.
+					b.b2 = true;
+					object->position()->x += perf;
+					break;
+				}
+			}
+		}
+		return b;
+	}
+
+	/// <summary>
+	/// Güvenli bir þekilde y pozisyonunda haraket iþlemi.
+	/// Eðer ilerlerken parametredeki id ile bir obje çarpýþtýysa eklemeyi durdurur ve eklenmemiþ hale getirir.
+	/// </summary>
+	/// <param name="w">Ýlerlenecek birim sayýsý</param>
+	/// <param name="perf">Performans sayýsý. Kaçar kaçar kontrol edeceði yani</param>
+	/// <param name="willControl">Kontrol edilecek id</param>
+	/// <returns></returns>
+	tValBool addY(float w, float perf, std::string willControl) {
+		tValBool b = { false, false };
+		if (w > 0) {
+			for (float i = 0; i < w; i += perf) {
+				float prevPos = object->position()->y;
+				object->position()->y += perf;
+				if (control(willControl)) {
+					//Burda çarpýþtý ve çarpýþmamýþ hale geri döndü.
+					b.b1 = true;
+					object->position()->y -= perf;
+					break;
+				}
+			}
+		}
+		else if (w < 0) {
+			for (float i = 0; i > w; i -= perf) {
+				float prevPos = object->position()->y;
+				object->position()->y -= perf;
+				if (control(willControl)) {
+					//Burda çarpýþtý ve çarpýþmamýþ hale geri döndü.
+					b.b2 = true;
+					object->position()->y += perf;
+					break;
+				}
+			}
+		}
+		return b;
+	}
+
+	/// <summary>
+	/// Force x ve y sini 0'a eþitler
+	/// </summary>
+	void forceClear() {
+		force.x = 0; force.y = 0;
+	}
+
+	/// <summary>
+	/// Eðer triggers deðiþkeni çok objeye eklenecekse Triggers deðiþkeni kod içinde tanýmlanýr, Triggerlar eklenir ve bu fonksiyon ile objeye atanýr.
+	/// </summary>
+	/// <param name="urTriggers">Triggers objen</param>
+	void setPhyTriggers(Triggers urTriggers) {
+		triggerControl = urTriggers;
+	}
+
+	/// <summary>
+	/// gameObject'in adresini döndürür
+	/// </summary>
+	/// <returns>physicsObject.gameObject</returns>
+	gameObject* getObject() {
+		return object;
+	}
+
+	/// <summary>
+	/// Objenin trigger deðiþkeninin adresini döndürür
+	/// </summary>
+	/// <returns>Objedeki Trigger</returns>
+	Trigger* getTrigger() {
+		return &trigger;
+	}
+};
+#endif
+
+
+//Bir dizide kaç tane UIELEMENT olduðunu belirtmemiz istenir.
+//Bu þekilde kütüphaneyi yapan deðil kütüphaneyi kullanan dizilerin kaç tane olduðunu belirtmiþ olur.
+#ifndef UIELEMENTS
+#error UIELEMENTS'i define edip bir sayý belirtmen gerekiyor.
+//Bu UIELEMENTS programýnda kaç tane UI element'i kullanacaðýný belirtir.
+#else
+/// <summary>
+/// UI CLASS UI objelerini oluþturmaya çizmeye ve kontrolü saðlar
+/// </summary>
+class UI {
+private:
+	sf::Font font;
+	sf::Text texts[UIELEMENTS];
+	std::string id;
+	sf::RenderWindow* window;
+public:
+	/// <summary>
+	/// UI Sýnýfý oluþturucusu
+	/// </summary>
+	/// <param name="fontFilePath">Fontun dosya yolu</param>
+	/// <param name="fontId">Font yüklenirken kontrol için komut satýrýnda gözükecek olan id</param>
+	/// <param name="urWindow">Çizilecek olan pencerenin adresi</param>
+	UI(sf::String fontFilePath, std::string fontId, sf::RenderWindow* urWindow) {
+		font = setFont(fontFilePath, fontId);
+		id = fontId;
+		window = urWindow;
+	}
+	/// <summary>
+	/// Dizideki herhangi bir objenin verilerini girmeye yarar. Bu yeni bir obje oluþturmada olabilir. Makismum UIELEMENTS kadar obje girilebilir.
+	/// </summary>
+	/// <param name="array">Kaçýncý obje</param>
+	/// <param name="string">Yazý</param>
+	/// <param name="posX">Pozisyon X</param>
+	/// <param name="posY">Pozisyon Y</param>
+	void setObj(int array, sf::String string, int posX, int posY) {
+		texts[array].setString(string);
+		texts[array].setFont(font);
+		texts[array].setPosition(posX, posY);
+	}
+
+	/// <summary>
+	/// Herhangi bir Text objesinin adresini alma.
+	/// </summary>
+	/// <param name="array">Dizideki kaçýncý</param>
+	/// <returns>Seçtiðimiz objenin adresini döndürür</returns>
+	sf::Text* getText(int array) {
+		return &texts[array];
+	}
+
+	/// <summary>
+	/// UI Objelerini ekrana çizdirir.
+	/// </summary>
+	void draw() {
+		for (int i = 0; i < UIELEMENTS; i++) {
+			window->draw(texts[i]);
+		}
+	}
+};
+#endif
+
+//Yukardakilerle benzer þeyler
+#ifndef MULTIS
+#error MultiDraw için MULTIlerin max kaç tane olmasýný istediðini giriniz. (define MULTIS)
+#else
+/// <summary>
+/// Çoklu obje çizimleri için sýnýf
+/// </summary>
+class MultiDraw {
+private:
+	physicsObject* objects[MULTIS];
+	int nextMulti = 0;
+public:
+	/// <summary>
+	/// Çizilecek þeylere bir physicsObject atar
+	/// </summary>
+	/// <param name="object">Fizik Objesi</param>
+	void addObject(physicsObject* object) {
+		if (nextMulti == MULTIS) {
+			std::cout << std::endl << "Maksimum MULTIS";
+		}
+		else {
+			objects[nextMulti] = object;
+			nextMulti++;
+		}
+	}
+	/// <summary>
+	/// Ekrana çizdirir
+	/// </summary>
+	void draw() {
+		for (int i = 0; i < nextMulti; i++) {
+			objects[i]->draw();
+		}
+	}
+
+};
+
+#endif
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,111 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+
+#define UIELEMENTS 15
+#define TRIGGERCONTROL 50
+#define MULTIS 50
+
+#include "RENG/reng.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+
+
+
+int main()
+{
+	std::string zeminId = "zemin";
+
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Burkeng");
+	setWindow(window,120);
+	//TEXTURE
+	Texturee burkaTexture;
+	setTexturee(&burkaTexture, 300, 300, "Burka", "res/imgs/burka.jpg");
+	Texturee mouseTexture;
+	setTexturee(&mouseTexture, 540, 540, "Mouse", "res/imgs/mouse.png");
+	Texturee grassTexture;
+	setTexturee(&grassTexture, 450, 338, "Grass", "res/imgs/grass.jpg");
+	Texturee duvarTexture;
+	setTexturee(&duvarTexture, 400, 271, "Duvar", "res/imgs/duvar.jpg");
+	//GAMEOBJECT
+	gameObject mouse(mouseTexture,10,10,0.10,0.10,&window);
+	gameObject burkaObj(burkaTexture,140,10,0.50,0.50,&window);
+	gameObject grassObj(grassTexture, 50, 300, 0.50, 0.50, &window);
+	gameObject grass2Obj(grassTexture, 200, 300, 0.50, 0.50, &window);
+	gameObject grass3Obj(grassTexture, 400, 300, 0.50, 0.50, &window);
+	gameObject duvarObj(duvarTexture, 400,100, 0.50, 0.50, &window);
+	physicsObject burka(&burkaObj,true,1.0,"BURKA");
+	physicsObject grass (&grassObj,false,0.0,"Cimen");
+	physicsObject grass2(&grass2Obj, false, 0.0, "Cimen2");
+	physicsObject grass3(&grass3Obj, false, 0.0, "Cimen3");
+	physicsObject duvar(&duvarObj, false, 0.0, "Duvar");
+
+	MultiDraw draw;
+	draw.addObject(&burka); draw.addObject(&grass); draw.addObject(&grass2); draw.addObject(&grass3); draw.addObject(&duvar);
+
+	Triggers zeminler;
+	setTriggers(&zeminler);
+	addNewTrigger(&zeminler, grass.getTrigger(), &zeminId);
+	addNewTrigger(&zeminler, grass2.getTrigger(), &zeminId);
+	addNewTrigger(&zeminler, grass3.getTrigger(), &zeminId);
+	addNewTrigger(&zeminler, duvar.getTrigger(), &zeminId);
+
+	burka.setPhyTriggers(zeminler);
+
+	UI ui("res/fonts/batmfo__.ttf","Ana",&window);
+	ui.setObj(0,"Karakter", 10, 10);
+
+	bool sol = false, sag = false;
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+			else if (event.type == sf::Event::MouseMoved) {
+				mouse.position()->x = event.mouseMove.x; mouse.position()->y = event.mouseMove.y;
+				ui.getText(0)->setPosition(event.mouseMove.x, event.mouseMove.y+300*0.1);
+			}
+			else if (event.type == sf::Event::KeyPressed) {
+				switch (event.key.code) {
+				case sf::Keyboard::D:
+					sag = true;
+					break;
+				case sf::Keyboard::A:
+					sol = true;
+					break;
+				}
+			}
+			else if (event.type == sf::Event::KeyReleased) {
+				switch (event.key.code) {
+				case sf::Keyboard::D:
+					sag = false;
+					break;
+				case sf::Keyboard::A:
+					sol = false;
+					break;
+				}
+			}
+		}
+		grass.setTrigger();
+		grass2.setTrigger();
+		grass3.setTrigger(),
+		burka.setTrigger();
+		burka.gravity(zeminId);
+		if (sag) {
+			burka.addX(5.0,1.0,zeminId);
+		}
+		else if (sol) {
+			burka.addX(-5.0, 1.0, zeminId);
+		}
+		window.clear(sf::Color::Black);
+		draw.draw();
+		ui.draw();
+		mouse.draw();
+		window.display();
+	}
+
+	return 0;
+
+}
