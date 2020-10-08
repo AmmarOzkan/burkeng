2dcc8bfbab73f88e192e8cfaf1cbca3bcc547900
AmmarOzkan <61326832+AmmarOzkan@users.noreply.github.com>
2020-10-09 01:13:03 +0300

    Yeni texture creator bit sayısı belirlenebilir...

--- Examples/textureCreator.cpp
+++ Examples/textureCreator.cpp
@@ -24,11 +24,18 @@
 
 #define PERFORMANCE 1.0
 
-
-int main()
+#include <sstream>
+int main(int argv, char *argc[])
 {
+	int bit = 4;
+	std::string filePath = "file.mg";
+	if (argv == 2) filePath = argc[1];
+	else if (argv == 3) {
+		filePath = argc[1]; 
+		std::stringstream s(argc[2]);
+		s >> bit;
+	}
 
-	std::string filePath = "data/character.mg";
 
 	srand(time(0));
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Test");
@@ -38,10 +45,10 @@ int main()
 	valueXY non = { 0,0 };
 
 	sf::Image img;
-	char fileChar[8][8];
-	img.create(8, 8, sf::Color::Transparent);
-	for (int x = 0; x < 8; x++) {
-		for (int y = 0; y < 8; y++) {
+	char fileChar[64][64];
+	img.create(bit, bit, sf::Color::Transparent);
+	for (int x = 0; x < bit; x++) {
+		for (int y = 0; y < bit; y++) {
 			fileChar[x][y] = '-';
 			img.setPixel(x, y, sf::Color::Transparent);
 		}
@@ -49,7 +56,7 @@ int main()
 	sf::Texture txtr;
 	txtr.loadFromImage(img);
 	Texturee cursorTexture = setTexturee(100, 100, "cursor", txtr);
-	gameObject cursor(cursorTexture,0,0,100,100,true,&window,&camera);
+	gameObject cursor(cursorTexture, 0, 0, 100, 100, true, &window, &camera);
 	int color = 1;
 	while (window.isOpen()) {
 
@@ -87,15 +94,16 @@ int main()
 			if (event.type == sf::Event::MouseMoved) {
 				cursor.position()->x = event.mouseMove.x;
 				cursor.position()->y = event.mouseMove.y;
-			}else if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::C){
+			}
+			else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::C) {
 				color++;
 				if (color > 5) color = -2;
 			}
 			else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::S) {
 				std::ofstream file(filePath, std::ios::app);
 				if (file) {
-					for (int y = 0; y < 8; y++) {
-						for (int x = 0; x < 8; x++) {
+					for (int y = 0; y < bit; y++) {
+						for (int x = 0; x < bit; x++) {
 							file << fileChar[x][y];
 						}
 					}
@@ -103,20 +111,20 @@ int main()
 				}
 			}
 			else if (event.type == sf::Event::MouseButtonPressed) {
-				int x = event.mouseButton.x/100;
-				int y = event.mouseButton.y / 100;
+				int x = event.mouseButton.x / (WIDTH/bit);
+				int y = event.mouseButton.y / (HEIGHT/bit);
 				std::cout << x << " " << y << std::endl;
-				if (color == 1) {img.setPixel(x, y, sf::Color::Red);fileChar[x][y] = '1';}
-				else if (color == 2) {img.setPixel(x, y, sf::Color::Blue);fileChar[x][y] = '2';}
-				else if (color == 3) {img.setPixel(x, y, sf::Color::Green);fileChar[x][y] = '3';}
-				else if (color == 4) {img.setPixel(x, y, sf::Color::Yellow);fileChar[x][y] = '4';}
-				else if (color == 5) {img.setPixel(x, y, sf::Color::Magenta);fileChar[x][y] = '5';}
-				else if (color == 0) {img.setPixel(x, y, sf::Color::Transparent);fileChar[x][y] = '-';}
-				else if (color == -1) {img.setPixel(x, y, sf::Color::Color(100, 100, 100));fileChar[x][y] = '*';}
-				else if (color == -2) {img.setPixel(x, y, sf::Color::Color(255, 255, 255));fileChar[x][y] = '+';}
+				if (color == 1) { img.setPixel(x, y, sf::Color::Red); fileChar[x][y] = '1'; }
+				else if (color == 2) { img.setPixel(x, y, sf::Color::Blue); fileChar[x][y] = '2'; }
+				else if (color == 3) { img.setPixel(x, y, sf::Color::Green); fileChar[x][y] = '3'; }
+				else if (color == 4) { img.setPixel(x, y, sf::Color::Yellow); fileChar[x][y] = '4'; }
+				else if (color == 5) { img.setPixel(x, y, sf::Color::Magenta); fileChar[x][y] = '5'; }
+				else if (color == 0) { img.setPixel(x, y, sf::Color::Transparent); fileChar[x][y] = '-'; }
+				else if (color == -1) { img.setPixel(x, y, sf::Color::Color(100, 100, 100)); fileChar[x][y] = '*'; }
+				else if (color == -2) { img.setPixel(x, y, sf::Color::Color(255, 255, 255)); fileChar[x][y] = '+'; }
 			}
 		}
-		window.clear(sf::Color::Color(50,25,25));
+		window.clear(sf::Color::Color(50, 25, 25));
 		std::string cmd;
 		int posX, posY;
 		std::string color;
@@ -138,7 +146,7 @@ int main()
 		}
 
 		txtr.loadFromImage(img);
-		Texturee t = setTexturee(8, 8, "d", txtr);
+		Texturee t = setTexturee(bit, bit, "d", txtr);
 		gameObject gm(t, 0, 0, WIDTH, HEIGHT, true, &window, &camera);
 		gm.draw();
 		cursor.draw();
