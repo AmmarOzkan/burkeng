9103c3f58fec8df7e5710bfbf2292a0c66bf26de
AmmarOzkan <61326832+AmmarOzkan@users.noreply.github.com>
2020-10-25 03:45:32 +0300

    Yeni çarpışma sistemi ve ufak bir bugfix

--- bureng_physics.h
+++ bureng_physics.h
@@ -13,56 +13,67 @@
 class Physics {
 private:
 	Vector2 force;
-	gameObject *gmo;
+	gameObject *gameobject;
 	Trigger *willControl[100], trigger;
 	int triggerControlCounter;
 public:
-	Physics(gameObject *urObject)
-		: gmo(urObject)
-	{
-		trigger = getTriggerFor(gmo);
-		force.x = 0; force.y = 0; triggerControlCounter = -1;
+	void resetTrigger() {
+		trigger = getTriggerFor(gameobject);
 	}
 
+
+	Physics(gameObject *urObject)
+		: gameobject(urObject)
+	{
+		resetTrigger();
+		force.x = 0; force.y = 0; triggerControlCounter = 0;
+	}
+
+	
+
 	void moveX(float willMove) {
-		Vector2 position = gmo->position()[0];
+		resetTrigger();
+		Vector2 position = gameobject->position()[0];
 		position.x += willMove;
 		TriggerId id = { "","" };
-		trigger = getTriggerFor(gmo);
+		trigger = getTriggerFor(gameobject);
 		for (int i = 0; i < triggerControlCounter; i++) {
-			setTrigger(&trigger, position.x, position.y, gmo->getTexturee()->width, gmo->getTexturee()->height, gmo->getScale()->x, gmo->getScale()->y, trigger.id, trigger.publicId);
+			setTrigger(&trigger, position.x, position.y, gameobject->getTexturee()->width, gameobject->getTexturee()->height, gameobject->getScale()->x, gameobject->getScale()->y, trigger.id, trigger.publicId);
 			if (isColliding(trigger, willControl[i][0])) {
 				id = { willControl[i]->id, willControl[i]->publicId };
-				if (willMove > 0) position.x = willControl[i]->pos.x - (gmo->getScale()->x * gmo->getTexturee()->width);
+				if (willMove > 0) position.x = willControl[i]->pos.x - (gameobject->getScale()->x * gameobject->getTexturee()->width);
 				else if (willMove < 0) position.x = willControl[i]->pos.x + (willControl[i]->scale.x);
 			}
 		}
 		std::cout << position.x<<std::endl;
-		gmo->setPosition(position);
+		gameobject->setPosition(position);
 	}
 
 	void moveY(float willMove) {
-		Vector2 position = gmo->position()[0];
+		resetTrigger();
+		Vector2 position = gameobject->position()[0];
 		position.y += willMove;
 		TriggerId id = { "","" };
-		trigger = getTriggerFor(gmo);
+		trigger = getTriggerFor(gameobject);
 		for (int i = 0; i < triggerControlCounter; i++) {
-			setTrigger(&trigger, position.x, position.y, gmo->getTexturee()->width, gmo->getTexturee()->height, gmo->getScale()->x, gmo->getScale()->y, trigger.id, trigger.publicId);
+			setTrigger(&trigger, position.x, position.y, gameobject->getTexturee()->width, gameobject->getTexturee()->height, gameobject->getScale()->x, gameobject->getScale()->y, trigger.id, trigger.publicId);
 			if (isColliding(trigger, willControl[i][0])) {
 				id = { willControl[i]->id, willControl[i]->publicId };
-				if (willMove > 0) position.y = position.y - (gmo->getScale()->y * gmo->getTexturee()->width);
+				if (willMove > 0) position.y = position.y - (gameobject->getScale()->y * gameobject->getTexturee()->width);
 				else if (willMove < 0) position.y = willControl[i]->pos.y + (willControl[i]->scale.y);
 			}
 		}
 		std::cout << position.y << std::endl;
-		gmo->setPosition(position);
+		gameobject->setPosition(position);
 	}
 
-	void move(Vector2 v) { moveX(v.x); moveY(v.y); }
+	void move(Vector2 v) { moveX(v.x);moveY(v.y);}
 	void phy() {move(force);force = { 0,0 };}
 
-	void phyDraw() { phy(); gmo->draw(); }
+	void phyDraw() {phy();gameobject->draw();}
+	gameObject* gm() {return gameobject;}
 
+	void setGm(gameObject* gmo) { gameobject = gmo; }
 
 	void addTrigger(Trigger *trigger) {
 		willControl[triggerControlCounter++] = trigger;
